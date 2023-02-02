7cb70859b87ba471cf31f913edf0f114cf550a82
Hosseinreza <hrakbari77@gmail.com>
2023-02-02 08:47:28 +0330

    Conditions on days

--- app/app.js
+++ app/app.js
@@ -1,10 +1,17 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const { response } = require("express");
 
 const app = express();
 
 app.get("/", function( req, res){
-    res.send("hello");
+    var today = new Date();
+    if( today.getDay() === 6 || today.getDay() === 0){
+        res.send("It's weekend");
+    }else{
+        res.send("It's Work day");
+    }
+    // res.send("hello");
 });
 
 
