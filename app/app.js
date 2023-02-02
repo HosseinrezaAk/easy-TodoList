42a2726b8dd2bf4cfe3bf01688b7bd3ff3878a61
Hosseinreza <hrakbari77@gmail.com>
2023-02-02 10:02:58 +0330

    Add switch case for days

--- app/app.js
+++ app/app.js
@@ -8,12 +8,36 @@ app.use("view engine", "ejs");
 app.get("/", function( req, res){
     var today = new Date();
     var currentDay = today.getDay();
-    if( currentDay === 6 || currentDay === 0){
-        res.send("It's weekend");
-    }else{
-        res.send("It's Work day");
+    var day = "";
+    switch (currentDay){
+        case 0:
+            day = "Sunday";
+            break;
+        case 1:
+            day = "Monday";
+            break;
+        case 2:
+            day = "Tuesday";
+            break;
+        case 3:
+            day = "Wednesday";
+            break;
+        case 4:
+            day = "Thursday";
+            break;
+        case 5:
+            day = "Friday";
+            break;
+        case 6:
+            day = "Saturday";
+            break;
+        default:
+            console.log("Error: current day is equal to : " + currentDay);
+
     }
-    // res.send("hello");
+    res.render("list", {
+        kindOfDay : day
+    })
 });
 
 
