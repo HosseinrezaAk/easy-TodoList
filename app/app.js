31973f8b57f2852074a771b385b9da07ef0e2072
Hosseinreza <hrakbari77@gmail.com>
2023-02-14 18:40:58 +0330

    Add itemSchema

--- app/app.js
+++ app/app.js
@@ -1,14 +1,20 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const date = require(__dirname + "/date.js");
-
+const mongoose = require("mongoose");
 const app = express();
 app.set('view engine', 'ejs'); // to pass data to front. check document
 app.use(bodyParser.urlencoded({extended : true}));
 app.use(express.static("public"));
 
-const items = [];
-const workItems = [];
+mongoose.set('strictQuery', false);
+mongoose.connect("mongodb://localhost:27017/fruitsDB");
+
+const itemSchema = {
+    name: String
+};
+
+const Item = mongoose.model("Item",itemSchema);
 
 app.get("/", function( req, res){
     let day = date.getDate();
