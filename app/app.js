271d36d24a5394618474f41ea8d7c8c362ff84b1
Hosseinreza <hrakbari77@gmail.com>
2023-02-15 20:39:32 +0330

    Add delete for each task

--- app/app.js
+++ app/app.js
@@ -56,6 +56,16 @@ app.post("/", function( req, res){
     res.redirect("/");
     
 });
+app.post("/delete", function(req, res){
+    const checkedItemId = req.body.checkbox
+
+    Item.findByIdAndRemove(checkedItemId, function(err){
+        if(!err){
+            console.log("successfully deleted checked item.");
+        }
+    })
+    res.redirect("/");
+});
 app.get("/work" , function ( req, res){
     res.render(
         "list",
