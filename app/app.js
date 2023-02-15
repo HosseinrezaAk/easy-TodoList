fa76cef0a2d0bce2e8c9862452d7f98416e107b7
Hosseinreza <hrakbari77@gmail.com>
2023-02-15 19:39:02 +0330

    Add new item post

--- app/app.js
+++ app/app.js
@@ -48,15 +48,12 @@ app.get("/", function( req, res){
     
 });
 app.post("/", function( req, res){
-    let item = req.body.newItem;
-    console.log(req.body);
-    if( req.body.list === "Work List"){
-        workItems.push(item);
-        res.redirect("/work");
-    }else{
-        items.push(item);
-        res.redirect("/");
-    }
+    const itemName = req.body.newItem;
+    const item = new Item({
+        name: itemName
+    });
+    item.save();
+    res.redirect("/");
     
 });
 app.get("/work" , function ( req, res){
